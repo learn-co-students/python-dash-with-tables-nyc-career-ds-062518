d404f62b3e0227f5a0d292cfca581a5318781a65
mattymecks <mcmecoli@gmail.com>
2018-07-17 17:13:26 -0400

    Done.

--- dashwithtables/__init__.py
+++ dashwithtables/__init__.py
@@ -1,17 +1,40 @@
-#import dash
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from dashwithtables.food_interest_data import data
 
-#import dash core components as dcc
+app = dash.Dash(__name__, url_base_pathname ='/' )
 
-#import dash html components as html
+def generate_table(table_data=data):
+    return html.Tr(id='food-table', children=
+    [html.Tr(id='headers', children=[html.Th(col) for col in table_data[0].keys()])]
+    +
+    [html.Tr(id='row-data', children=[
+    html.Td(data_dict[column]) for column in data_dict.keys()
+    ]) for data_dict in table_data])
 
-# import Input, Output from dash.dependencies for callback functions
+app.layout = html.Div(children=[
+    dcc.Dropdown(
+        id = 'sort-by-selector',
+        options =[
+            {'label': 'Country', 'value': 'Country'},
+            {'label': 'Pho', 'value': 'Pho'},
+            {'label': 'Ramen', 'value': 'Ramen'},
+            {'label': 'Soba', 'value': 'Soba'}
+        ],
+        value="Country"
+    ),
+    html.H3("Interest in Pho, Ramen, and Soba by Country according to Google Search from 01/2004 - 06/2018"),
+    html.Div(id='table-container')
+])
 
 
-
-
-# import data about interest of pho/ramen/soba
-
-
-
-
-# create dash app and make path '/'
+@app.callback(
+    Output('table-container', 'children'),
+    [Input('sort-by-selector', 'value')]
+)
+def sort_table(input_value):
+    global data
+    sorted_data = sorted(data, key=lambda datum: datum[input_value])
+    return generate_table(sorted_data)
